4c017b31b88ee92e16cf283d2ba8195cb715edda
Szilvia Deahl <szilvia.deahl@yahoo.com>
2015-08-10 16:40:20 -0600

    the editor can accumulate multiple lines

--- spec/editorSpec.js
+++ spec/editorSpec.js
@@ -6,4 +6,14 @@ describe('Editor', function () {
     editor.write("Once upon a time at Galvanize...")
     expect(editor.lines[0]).toEqual("Once upon a time at Galvanize...");
   });
+
+  it('an editor can accumulate multiple lines', function () {
+    var editor = new Editor();
+    editor.write("hello");
+    editor.write("goodbye");
+    editor.write("greetigns");
+    expect(editor.lines.length).toEqual(3)
+    editor.write("I am four!");
+    expect(editor.lines.length).toEqual(4);
+  } )
 });
